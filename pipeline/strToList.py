40999766729259fa8b146e0b08dab4ff4a29521d
svetik <petrushyna@gmail.com>
2020-02-11 15:52:56 +0100

    strToList module

---
+++ pipeline/strToList.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+def strToList(list_l, splitSymbol):
+    """
+    @param
+
+    list_l      =  input string in form ['4,6,6']
+    splitSymbol =  is the symbol between future list elements
+    """
+
+    list_l = list_l.split(splitSymbol)
+    temp = list()
+    for l in list_l: 
+        l = l.replace("[",'').replace("]",'').replace("'", '').replace(" ", '')
+        temp.append(l)
+    return temp
+
+print(strToList('[4,5,6]', ','))
